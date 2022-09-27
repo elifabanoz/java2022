5f0537140810dded5ea780212eb1f507f669da7e
elifabanoz <113616850+elifabanoz@users.noreply.github.com>
2022-09-28 02:02:23 +0300

    Add files via upload

---
+++ javademos/HelloWorldMain.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+// intelligence : ctrl + space ile getirilen örnekler bunu java değil ide yapar.
+        System.out.println("Merhaba Java!");
+        System.out.println("Hoşçakal Ödev! :D ");
+    }
+}
