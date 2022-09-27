c637b1ec70887328b1e5085202aa450c7105b8ea
elifabanoz <113616850+elifabanoz@users.noreply.github.com>
2022-09-28 02:10:06 +0300

    Delete javademos/javaHelloWorld directory

--- javademos/javaHelloWorld/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-// intelligence : ctrl + space ile getirilen örnekler bunu java değil ide yapar.
-        System.out.println("Merhaba Java!");
-        System.out.println("Hoşçakal Ödev! :D ");
-    }
-}
