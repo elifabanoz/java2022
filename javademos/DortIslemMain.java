f84c32f120069086f86c563a61ca55e73a1fd8e1
elifabanoz <113616850+elifabanoz@users.noreply.github.com>
2022-09-28 03:26:20 +0300

    Delete DortIslemMain.java

--- javademos/DortIslemMain.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Dortislem dortislem= new Dortislem();
-        int sonuc = dortislem.Topla(1,2);
-        System.out.println(sonuc);
-    }
-}
